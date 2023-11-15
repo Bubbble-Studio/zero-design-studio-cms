2ab2afd7cba5824d6782deb660719355afea1d32
Anurag Pal <apal895@gmail.com>
2023-11-15 11:45:57 +0530

    added sendgrid

--- config/plugins.ts
+++ config/plugins.ts
@@ -14,16 +14,16 @@ export default ({ env }) => ({
       },
     },
   },
-  // email: {
-  //   config: {
-  //     provider: "sendgrid",
-  //     providerOptions: {
-  //       apiKey: env("SENDGRID_API_KEY"),
-  //     },
-  //     settings: {
-  //       defaultFrom: env("SENDGRID_DEFAULT_FROM"),
-  //       defaultReplyTo: env("SENDGRID_DEFAULT_REPLY_TO"),
-  //     },
-  //   },
-  // },
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: env("SENDGRID_DEFAULT_FROM"),
+        defaultReplyTo: env("SENDGRID_DEFAULT_REPLY_TO"),
+      },
+    },
+  },
 });
