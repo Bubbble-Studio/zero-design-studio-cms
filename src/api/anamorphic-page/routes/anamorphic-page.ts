beb833743fd67dfe8bf20aafb5b9233d52dc453e
Anurag Pal <apal895@gmail.com>
2025-05-09 01:24:38 +0530

    added anamorphic page

---
+++ src/api/anamorphic-page/routes/anamorphic-page.ts
@@ -0,0 +1,7 @@
+/**
+ * anamorphic-page router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::anamorphic-page.anamorphic-page');
---
+++ src/api/anamorphic-page/services/anamorphic-page.ts
@@ -0,0 +1,7 @@
+/**
+ * anamorphic-page service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::anamorphic-page.anamorphic-page');
