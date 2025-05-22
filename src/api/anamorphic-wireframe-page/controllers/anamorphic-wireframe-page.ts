efd93c8e0ff63a0b77cc03c643b219e1e94186cf
Anurag Pal <apal895@gmail.com>
2025-05-22 16:31:39 +0530

    removed wireframe page

--- src/api/anamorphic-wireframe-page/controllers/anamorphic-wireframe-page.ts
+++
@@ -1,7 +0,0 @@
-/**
- * anamorphic-wireframe-page controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::anamorphic-wireframe-page.anamorphic-wireframe-page');
--- src/api/anamorphic-wireframe-page/services/anamorphic-wireframe-page.ts
+++
@@ -1,7 +0,0 @@
-/**
- * anamorphic-wireframe-page service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::anamorphic-wireframe-page.anamorphic-wireframe-page');
